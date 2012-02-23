2ecdc22630474bd46a46c661fb3855f045a29bf3
Tuomas Starck <dev@starck.fi>
2012-02-24 01:31:34 +0200

    Armoton tornitustesti.

--- test/sakki/CastleTest.java
+++ test/sakki/CastleTest.java
@@ -50,4 +50,53 @@ public class CastleTest {
         assertTrue(castle.index(foo) == 1);
         assertTrue(castle.index(bar) == 2);
     }
+
+    /**
+     * Castling integration tests.
+     */
+    @Test
+    public void castlingIntegration() {
+        Chess game;
+
+        String exception;
+        String expectation = "Kings passage must be free";
+
+        String foo = "rnbqk2r/pppp1ppp/5n2/2b5/3PPp2/3B1N2/PPP3PP/RNBQK2R b KQkq - 4 5";
+        String bar = "r3k2r/p1p2ppp/1pn5/4Nb1q/4BB2/2NQ4/PPP3PP/R3K2R w KQkq - 5 13";
+
+        game = new Chess(foo);
+
+        try {
+            game.move("0-0");
+            game.move("0-0");
+        }
+        catch (MoveException me) {
+            assertTrue(false);
+        }
+
+        exception = "";
+        game = new Chess(bar);
+
+        try {
+            game.move("0-0-0");
+        }
+        catch (MoveException me) {
+            exception = me.toString();
+        }
+
+        assertEquals(expectation, exception);
+
+        exception = "";
+        game = new Chess(bar);
+
+        try {
+            game.move("Nxc6");
+            game.move("0-0-0");
+        }
+        catch (MoveException me) {
+            exception = me.toString();
+        }
+
+        assertEquals(expectation, exception);
+    }
 }
