b6eb3e6f73d39d066d28e3675412ce1401a8bb9c
Tuomas Starck <dev@starck.fi>
2012-05-02 19:14:36 +0300

    Refaktoroitu ja paranneltu tornitustesti.

--- test/sakki/CastleTest.java
+++ test/sakki/CastleTest.java
@@ -12,91 +12,81 @@ public class CastleTest {
     }
 
     /**
-     * Castle 1st: Testing parameterless constructor,
-     * toString() and a bunch of methods.
+     * Testing parameterless constructor
+     * and disable() method.
      */
     @Test
     public void disableCastlingOptions() {
         Castle castle = new Castle();
         castle.disable("Kq");
-        assertEquals(castle.toString(), "Qk");
+        assertEquals("Qk", castle.toString());
         castle.disable("qqq");
-        assertEquals(castle.toString(), "Qk");
+        assertEquals("Qk", castle.toString());
         castle.disable("Q");
-        assertEquals(castle.toString(), "k");
+        assertEquals("k",  castle.toString());
     }
 
     /**
-     * Castle 2nd: Testing String constructor, toString()
-     * and other stuff.
+     * Testing String constructor and
+     * disable() method.
      */
     @Test
-    public void noCastlingAllowed() {
-        Castle castle = new Castle("kongkku");
-        assertEquals(castle.toString(), "k");
+    public void castlingStringHandling() {
+        Castle castle = new Castle("Klonkku");
+        assertEquals("Kk", castle.toString());
         castle.disable("k");
-        assertEquals(castle.toString(), "-");
+        assertEquals("K", castle.toString());
+        castle.disable("K");
+        assertEquals("-", castle.toString());
     }
 
     /**
-     * Castle 3rd: Testing index() method.
+     * Testing index() method.
      */
     @Test
     public void testIndex() throws MoveException {
         Castle castle = new Castle();
         Move foo = new Move("0-0-0+", Side.w);
         Move bar = new Move("0-0", Side.b);
-
         assertTrue(castle.index(foo) == 1);
         assertTrue(castle.index(bar) == 2);
     }
 
     /**
-     * Castling integration tests.
+     * Basic castling integration test.
      */
     @Test
     public void castlingIntegration() {
-        Chess game;
-
-        String exception;
-        String expectation = "Kings passage must be free";
-
-        String foo = "rnbqk2r/pppp1ppp/5n2/2b5/3PPp2/3B1N2/PPP3PP/RNBQK2R b KQkq - 4 5";
-        String bar = "r3k2r/p1p2ppp/1pn5/4Nb1q/4BB2/2NQ4/PPP3PP/R3K2R w KQkq - 5 13";
-
-        game = new Chess(foo);
+        String expectation = "rnbq1rk1/pppp1ppp/5n2/2b5/3PPp2/3B1N2/PPP3PP/RNBQ1RK1 b - - 6 6";
+        Chess game = new Chess("rnbqk2r/pppp1ppp/5n2/2b5/3PPp2/3B1N2/PPP3PP/RNBQK2R b KQkq - 4 5");
 
         try {
             game.move("0-0");
-            game.move("0-0");
+            game.move("O-O");
         }
         catch (MoveException me) {
             assertTrue(false);
         }
 
-        exception = "";
-        game = new Chess(bar);
+        assertEquals(expectation, game.toString());
+    }
 
-        try {
-            game.move("0-0-0");
-        }
-        catch (MoveException me) {
-            exception = me.toString();
-        }
+    /**
+     * Checked king test.
+     */
+    @Test(expected = MoveException.class)
+    public void checkedKing() throws MoveException {
+        Chess game = new Chess("r3k2r/p1p2ppp/1pn5/4Nb1q/4BB2/2NQ4/PPP3PP/R3K2R w KQkq - 5 13");
+        game.move("O-O-O");
+    }
 
-        assertEquals(expectation, exception);
-
-        exception = "";
-        game = new Chess(bar);
-
-        try {
-            game.move("Nxc6");
-            game.move("0-0-0");
-        }
-        catch (MoveException me) {
-            exception = me.toString();
-        }
-
-        assertEquals(expectation, exception);
+    /**
+     * Threatened passage test.
+     */
+    @Test(expected = MoveException.class)
+    public void threatenedPassage() throws MoveException {
+        Chess game = new Chess("r3k2r/p1p2ppp/1pn5/4Nb1q/4BB2/2NQ4/PPP3PP/R3K2R w KQkq - 5 13");
+        game.move("Bxc6");
+        game.move("0-0");
     }
 }
